8a4f32d5c87f7702c7fa4f956fbcb5233675c277
Kata Cseke <katacseke@gmail.com>
2021-03-26 13:58:49 +0200

    Meal creatinon page finished.

--- pages/api/meals/index.js
+++ pages/api/meals/index.js
@@ -1,11 +1,16 @@
 import nextConnect from 'next-connect';
 import validateResource from '../../../server/middleware/validateResource';
+import authorize, { hasRole } from '../../../server/middleware/authorize';
 import { mealService } from '../../../server/services';
 import mealCreationSchema from '../../../validation/mealCreationSchema';
 
 const validation = nextConnect().post('/api/meals', validateResource(mealCreationSchema));
+const authorization = nextConnect().post(
+  '/api/meals',
+  authorize(hasRole('restaurant'), hasRole('admin'))
+);
 
-const handler = nextConnect().use(validation);
+const handler = nextConnect().use(validation).use(authorization);
 
 handler.get(async (req, res) => {
   const meals = await mealService.getMeals();
@@ -19,7 +24,7 @@ handler.get(async (req, res) => {
 });
 
 handler.post(async (req, res) => {
-  const meal = await mealService.createMeal(req.body);
+  const meal = await mealService.createMeal({ ...req.body, restaurantId: req.user.id });
 
   if (!meal.success) {
     res.status(500).json({ error: meal.error });
