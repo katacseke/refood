e510c67b855b60ca5c1f6c3587fda99e2047f111
Kata Cseke <katacseke@gmail.com>
2021-03-27 12:06:10 +0200

    Created homepage section for restaurant application

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,37 @@
 import { useContext } from 'react';
+import { Button } from 'shards-react';
+import Router from 'next/router';
 import Layout from '../components/layout';
-import { mealService } from '../server/services';
 import AuthContext from '../context/authContext';
 
-const Home = ({ meals }) => {
+const Home = () => {
   const { user } = useContext(AuthContext);
 
   return (
     <Layout>
       <h1>Üdvözlünk {user && user.name}!</h1>
-      <h3>
+      <p className="highlightedText">
         Tudtad, hogy 2019-ben Románia 5 millió tonna ételfelesleget termelt? Most te is
         hozzájárulhatsz, hogy ez változhasson! Alkalmazásunk segítségével helyi vendéglők, kávézók
         fennmaradt termélei vásárolhatók meg!
-      </h3>
-      <p>Meals: {meals.map((meal) => meal.name).join(', ')}</p>
+      </p>
+
+      <h2>Partnereket keresünk</h2>
+      <p className="highlightedText">
+        Vendéglő, vagy kávézótulajdonos vagy? Itt regisztrálhatod a saját vállalkozásod, hogy
+        ezáltal is hozzájárulj a food waste csökkentéséhez!
+      </p>
+      <Button
+        onClick={() => {
+          Router.push('/restaurants/apply');
+        }}
+        className="align-self-end"
+        size="lg"
+      >
+        Jelentkezz
+      </Button>
     </Layout>
   );
 };
 
 export default Home;
-
-export async function getStaticProps() {
-  const meals = await mealService.getMeals();
-
-  return {
-    props: { meals: JSON.parse(JSON.stringify(meals.data)) },
-  };
-}
