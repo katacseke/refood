1e9d272f55f143b5edca7214f742d7216a20290d
Kata Cseke <katacseke@gmail.com>
2021-03-27 12:08:08 +0200

    Created application model and validation.

---
+++ server/models/restaurantApplication.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const restaurantApplicationSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
+  description: String,
+});
+
+mongoose.models = {};
+
+const Application = mongoose.model('Application', restaurantApplicationSchema);
+export default Application;
