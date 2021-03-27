62b47dc23a3c6d312ce50cdde2662a7b80113426
Kata Cseke <katacseke@gmail.com>
2021-03-27 21:56:21 +0200

    Created restaurant application form

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
+import { Toaster } from 'react-hot-toast';
 import '../styles/globals.scss';
 import { AuthProvider } from '../context/authContext';
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Toaster />
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
     </AuthProvider>
