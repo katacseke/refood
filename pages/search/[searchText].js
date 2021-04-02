f707cac351faa79fd73260c8be974e41261e1817
Kata Cseke <katacseke@gmail.com>
2021-04-02 12:55:19 +0300

    updated search result page with meal modal.

--- pages/search/[searchText].js
+++ pages/search/[searchText].js
@@ -1,23 +1,36 @@
+import { useState } from 'react';
 import { Container } from 'shards-react';
 import Layout from '../../components/layout';
 import Card from '../../components/card';
+import MealModal from '../../components/mealModal';
 import { mealService, restaurantService } from '../../server/services';
 
-const SearchResultPage = ({ meals, restaurants, text }) => (
-  <Layout>
-    <h1>Találatok a következőre: {text}</h1>
-    <Container className="d-flex flex-wrap align-content-md-stretch">
-      {meals.map((meal) => (
-        <Card data={meal} type="meal" />
-      ))}
-    </Container>
-    <Container className="d-flex flex-wrap align-content-md-stretch">
-      {restaurants.map((restaurant) => (
-        <Card data={restaurant} type="restaurant" />
-      ))}
-    </Container>
-  </Layout>
-);
+const SearchResultPage = ({ meals, restaurants, text }) => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedMeal, setSelectedMeal] = useState();
+
+  const showMeal = (meal) => {
+    setSelectedMeal(meal);
+    setModalOpen(true);
+  };
+
+  return (
+    <Layout>
+      <MealModal meal={selectedMeal} open={modalOpen} setOpen={setModalOpen} />
+      <h1>Találatok a következőre: {text}</h1>
+      <Container className="d-flex flex-wrap align-content-md-stretch">
+        {meals.map((meal) => (
+          <Card data={meal} type="meal" onClick={() => showMeal(meal)} />
+        ))}
+      </Container>
+      <Container className="d-flex flex-wrap align-content-md-stretch">
+        {restaurants.map((restaurant) => (
+          <Card data={restaurant} type="restaurant" />
+        ))}
+      </Container>
+    </Layout>
+  );
+};
 export default SearchResultPage;
 
 export async function getServerSideProps({ params }) {
