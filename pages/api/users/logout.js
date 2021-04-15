e99dd936249dbe929c02d500f3fe937f170e70df
Kata Cseke <katacseke@gmail.com>
2021-04-15 11:20:09 +0300

    Fixed logout bug.

--- pages/api/users/logout.js
+++ pages/api/users/logout.js
@@ -3,7 +3,10 @@ import nextConnect from 'next-connect';
 const handler = nextConnect();
 
 handler.post(async (req, res) => {
-  res.setHeader('Set-Cookie', 'access_token=deleted; expires=Thu, 01 Jan 1970 00:00:00 GMT');
+  res.setHeader(
+    'Set-Cookie',
+    'access_token=deleted; Path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT;  HttpOnly'
+  );
   res.status(204).send();
 });
 
