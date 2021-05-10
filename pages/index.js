8044b83291f0d92de8e49e5a366c1b4a92c980d3
Kata Cseke <katacseke@gmail.com>
2021-05-10 10:40:52 +0300

    Home page designed.

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,84 @@
-import { useContext } from 'react';
+import Image from 'next/image';
 import { Button } from 'shards-react';
-import Router from 'next/router';
-import Layout from '../components/layout';
-import AuthContext from '../context/authContext';
+import NavBar from '@components/navBar';
+import Link from 'next/link';
+import {
+  IoCashOutline,
+  IoEarthOutline,
+  IoHeartOutline,
+  IoLeafOutline,
+  IoRestaurantOutline,
+  IoWalkOutline,
+} from 'react-icons/io5';
+import styles from './home.module.scss';
 
-const Home = () => {
-  const { user } = useContext(AuthContext);
+const Feature = ({ icon, children }) => (
+  <li>
+    <div className={styles.iconContainer}>{icon}</div>
+    {children}
+  </li>
+);
 
-  return (
-    <Layout>
-      <h1>Üdvözlünk {user && user.name}!</h1>
-      <p className="highlightedText">
-        Tudtad, hogy 2019-ben Románia 5 millió tonna ételfelesleget termelt? Most te is
-        hozzájárulhatsz, hogy ez változhasson! Alkalmazásunk segítségével helyi vendéglők, kávézók
-        fennmaradt termélei vásárolhatók meg!
-      </p>
+const Home = () => (
+  <>
+    <NavBar />
+    <div className={styles.topContainer}>
+      <Image
+        src="/hermes-rivera-Ww8eQWjMJWk-unsplash.jpg"
+        alt=""
+        width={5465}
+        height={2285}
+        layout="responsive"
+      />
+      <div className={styles.aboveLeft}>
+        <h1 className={styles.title}>
+          <strong>Éhes vagy?</strong> <br />
+          Nézd meg az ajánlatainkat!
+        </h1>
+        <p className={styles.description}>
+          Tudtad, hogy 2019-ben Románia 5 millió tonna ételfelesleget termelt? Most te is
+          hozzájárulhatsz, hogy ez változhasson! Alkalmazásunk segítségével helyi vendéglők, kávézók
+          fennmaradt termélei vásárolhatók meg!
+        </p>
+        <Link href="/meals">
+          <Button tag="a" className="align-self-start mt-2" size="lg">
+            Tovább
+          </Button>
+        </Link>
+      </div>
+    </div>
 
-      <h2>Partnereket keresünk</h2>
-      <p className="highlightedText">
-        Vendéglő, vagy kávézótulajdonos vagy? Itt regisztrálhatod a saját vállalkozásod, hogy
-        ezáltal is hozzájárulj a food waste csökkentéséhez!
-      </p>
-      <Button
-        onClick={() => {
-          Router.push('/restaurants/apply');
-        }}
-        className="align-self-end"
-        size="lg"
-      >
-        Jelentkezz
-      </Button>
-    </Layout>
-  );
-};
+    <div className={styles.centerContainer}>
+      <Image src="/vegleges.jpg" alt="" width={2400} height={1000} layout="responsive" />
+      <ul className={styles.featureGrid}>
+        <Feature icon={<IoEarthOutline />}> Ételpazarlás-csökkentés </Feature>
+        <Feature icon={<IoCashOutline />}> Pénz megtakarítás </Feature>
+        <Feature icon={<IoLeafOutline />}> Kisebb ökológiai lábnyom </Feature>
+        <Feature icon={<IoRestaurantOutline />}> Finom falatok </Feature>
+        <Feature icon={<IoWalkOutline />}> Személyes átvétel </Feature>
+        <Feature icon={<IoHeartOutline />}> Adományozás </Feature>
+      </ul>
+    </div>
+
+    <div className={styles.joinContainer}>
+      <img src="/join.svg" alt="" />
+      <div className={styles.textbox}>
+        <h1 className={styles.title}>
+          <strong>Partnereket keresünk</strong>
+        </h1>
+        <p className="lead">
+          Vendéglő, vagy kávézótulajdonos vagy? Itt regisztrálhatod a saját vállalkozásod, hogy
+          ezáltal is hozzájárulj a food waste csökkentéséhez!
+        </p>
+
+        <Link href="/restaurants/apply">
+          <Button tag="a" className="align-self-start mt-2" size="lg">
+            Jelentkezz
+          </Button>
+        </Link>
+      </div>
+    </div>
+  </>
+);
 
 export default Home;
